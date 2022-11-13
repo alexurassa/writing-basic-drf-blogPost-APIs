d384ed0267cb173c6a7222423731617805143413
alexurassa <alexurassa03@gmail.com>
2022-11-13 06:47:30 +0300

    skey removed

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$8*ks=sg5*#l)=5*-qwlq1+-^t+eby7r=l=%f3mjcmt4@nqokv"
+SECRET_KEY = "YOUR SECRET KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
